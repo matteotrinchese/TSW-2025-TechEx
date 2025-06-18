04e80eb50c53cbe7e1bc0aff7718f42d1f580508
matteotrinchese <170922208+matteotrinchese@users.noreply.github.com>
2025-06-19 00:40:45 +0200

    Creazione tabella "Product" e inserimento primo elemento.

--- sql/database.sql
+++ sql/database.sql
@@ -9,5 +9,14 @@ CREATE TABLE IF NOT EXISTS User(
     is_admin        BOOLEAN NOT NULL
 );
 
-
+CREATE TABLE IF NOT EXISTS Product(
+    ID              INT AUTO_INCREMENT PRIMARY KEY,
+    Name            VARCHAR(255) NOT NULL,
+    Description     TEXT,
+    Brand           VARCHAR(255) NOT NULL,
+    Price           DECIMAL(10, 2) NOT NULL,        -- prezzo senza IVA
+    Type            VARCHAR(255) NOT NULL,
+    Quantity        INT NOT NULL DEFAULT 0 CHECK (Quantity >= 0),
+    VAT             DECIMAL(5, 2) DEFAULT 22.00 NOT NULL          -- es. 22.00 -> 22%
+);
 
